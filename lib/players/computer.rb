d0f09a05867dd5349088d94a9ef199746bb0f397
Roberto Garza <robertg4567@hotmail.com>
2018-08-10 18:05:53 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,7 +12,20 @@ module Players
       [6,4,2]
     ]
     def move(board)
-      "#{rand(1..9)}"
+      #"#{rand(1..9)}"
+      enemyToken = " "
+      if token == "X"
+        enemyToken = "O"
+      elsif token =="O"
+        enemyToken == "X"
+      end 
+      WIN_COMBINATIONS.each do |win_combination|
+        if (board.position(win_combination[0]) == enemToken &&
+            board.position(win_combination[1]) == enemToken ) ||
+           (board.position(win_combination[0]) == enemToken &&
+            board.position(win_combination[2]) == enemToken ) ||
+           (board.position(win_combination[1]) == enemToken &&
+            board.position(win_combination[2]) == enemToken ) 
     end
   end
 end
